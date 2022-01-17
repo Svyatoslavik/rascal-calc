48b229ed55f2304bf8dd627dfd56e838ce6f9e3a
Sviatoslav Mysachenko <svyatoslavik@gmail.com>
2022-01-17 13:53:17 +0200

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
 import './App.css';
 import { CalcHafeleWoodPro } from './components/CalcHafeleWoodPro/CalcHafeleWoodPro';
 import { PageCaclBlum } from './pages/PageCalcBlum/PageCalcBlum';
 import { PageHome } from './pages/PageHome/PageHome';
 
+
+
 function App() {
   return (
     <div className="container">
       <nav>
-        <Link to="/">Главная</Link>
+        <NavLink to="/">Главная</NavLink>
+        <NavLink to="/hafele">Hafele</NavLink>
+        <NavLink to="/blum">Blum</NavLink>
       </nav>
       <Routes>
         <Route path="/" element={<PageHome />} />
@@ -20,5 +25,14 @@ function App() {
     </div>
   );
 }
+
+const NavLink = styled(Link)`
+  padding: 20px;
+  color: blue;
+  text-decoration: none;
+  &:hover {
+    background: rgba(1,1,1,.1);
+  }
+`;
 
 export default App;
--- src/pages/PageHome/PageHome.tsx
+++ src/pages/PageHome/PageHome.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 
 export function PageHome() {
@@ -9,9 +10,15 @@ export function PageHome() {
                 <h2>Калькулятор для рассчета ящиков под направляющие</h2>
             </main>
             <nav>
-                <Link to="/hafele">Hafele Wood Pro</Link>
-                <Link to="/blum">Blum</Link>
+                <NavLink to="/hafele">Hafele Wood Pro</NavLink>
+                <NavLink to="/blum">Blum</NavLink>
             </nav>
         </>
     );
 }
+
+const NavLink = styled(Link)`
+    padding: 10px;
+    color: blue;
+    display: block;
+`;
